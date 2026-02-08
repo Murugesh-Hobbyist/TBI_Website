765c2b2eeb8dd7bd0df01bf7744f4dcb408b5782
Murugesh <75427145+Murugesh-Hobbyist@users.noreply.github.com>
2026-02-08 23:32:57 +0530

    Make ping show vendor autoload presence

--- public/ping.php
+++ public/ping.php
@@ -4,4 +4,4 @@
 // Remove once deployment is stable.
 header('Content-Type: text/plain; charset=UTF-8');
 echo "ok\n";
-
+echo 'vendor_autoload='.(file_exists(__DIR__.'/../vendor/autoload.php') ? 'yes' : 'no')."\n";
